67ead2f5cbd484c09664c070a49a340658e0b094
priya <priyanagaraju15@gmail.com>
2025-12-08 15:03:21 +0530

    Name of the city java files

---
+++ src/DataTypes/NameoftheCity_DataTypes.java
@@ -0,0 +1,12 @@
+package DataTypes;
+
+
+public class NameoftheCity_DataTypes {
+
+	public static void main(String[] args) {
+		// Name of the city :Vijayawada
+		String cityName = "Vijayawada";
+		System.out.println("name of the City:"+cityName);
+	}
+
+}
