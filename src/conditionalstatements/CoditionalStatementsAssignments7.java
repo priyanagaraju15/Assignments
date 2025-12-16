056b53240e895c4e4bff5d9c42a131f63e449bb5
priya <priyanagaraju15@gmail.com>
2025-12-16 16:58:14 +0530

    coditionalAssignments

---
+++ src/conditionalstatements/CoditionalStatementsAssignments7.java
@@ -0,0 +1,34 @@
+package conditionalstatements;
+
+public class CoditionalStatementsAssignments7 {
+
+	public static void main(String[] args) {
+		//Credit Score:
+		// o If the credit score is above 750, the loan is automatically approved.
+		int creditScore = 720;
+		int customerIncome = 55000;
+		boolean isEmployeed = true;
+		double debtToIncome = 35.0;
+		if(creditScore >750) {
+			System.out.println("Loan Approved");
+		}
+		else if(creditScore >650 && creditScore<750){
+			System.out.println("customer additional checks performing");
+			if(customerIncome>50000) {
+				if(isEmployeed) {
+					System.out.println("Loan further processing");
+					if (debtToIncome <40) {
+						System.out.println("Loan is Approved");
+					}
+					else if(debtToIncome>40) {
+						System.out.println("The Loan is Denied");
+					}	
+				}
+					
+		}else {
+					System.out.println("Loan Rejected as custmer is not employeed");
+				}
+	 }
+			
+ }
+}
